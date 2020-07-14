114e417f1534f615788ebf8a0cd527a499d1a56b
Aman Bhargava <56464156+BhargavaCreates@users.noreply.github.com>
2020-07-15 01:35:27 +0530

    test cases delete

--- TestBody.java
+++
@@ -1,8 +0,0 @@
-public class TestBody {
-	public static void main(String[] args){
-		Body earth = new Body(1.4960e+11,0.0000e+00,0.0000e+00,2.9800e+04,5.9740e+24,"earth.gif");
-		Body mars  = new Body(2.2790e+11,0.0000e+00,0.0000e+00,2.4100e+04,6.4190e+23,"mars.gif");
-		System.out.println("the force between Earth and mars is: ");
-		System.out.println(earth.calcForceExertedBy(mars));
-	}
-}
